26dfecae74003832d36f2d05d2589634759f8826
mikord45 <51155742+mikord45@users.noreply.github.com>
2023-06-19 01:26:00 +0200

    tutorial 2

--- SeleniumCSharpTutorials/BaseClass/BaseTest.cs
+++ SeleniumCSharpTutorials/BaseClass/BaseTest.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,22 +14,25 @@ namespace SeleniumCSharpTutorials.BaseClass
     public class BaseTest
     {
        public IWebDriver driver ;
+       public WebDriverWait wait;
 
-        [OneTimeSetUp]
+        [SetUp]
         public void Open()
         {
             driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             driver.Manage().Window.Maximize();
             driver.Url = "https://www.facebook.com/";
             IWebElement cookiesAcceptButton = driver.FindElement(By.XPath(".//*[@data-testid='cookie-policy-manage-dialog-accept-button']"));
             try
             {
                 cookiesAcceptButton.Click();
+                wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.XPath(".//*[@data-testid='cookie-policy-manage-dialog-accept-button']")));
             }
             catch { }
         }
 
-        [OneTimeTearDown]
+        [TearDown]
         public void Close()
         {
             driver.Quit();
--- SeleniumCSharpTutorials/TestClass.cs
+++ SeleniumCSharpTutorials/TestClass.cs
@@ -8,27 +8,38 @@ using SeleniumCSharpTutorials.BaseClass;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
 
 namespace SeleniumCSharpTutorials
 {
     [TestFixture]
     public class TestClass : BaseTest
     {
-        [Test]
+        [Test,Category("Smoke Testing")]
         public void TestMethod1()
         {
             IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
             emailTextField.SendKeys("Selenium C#");
+            IWebElement createAccountButton = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(".//*[@data-testid='open-registration-form-button']")));
+            createAccountButton.Click();
+            IWebElement monthDropdownList = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id='month']")));
+            SelectElement element = new SelectElement(monthDropdownList);
+            // differences in values, there is no "month" value in dropdown
+            element.SelectByIndex(0);
+            element.SelectByText("mar"); // different text in each different language
+            element.SelectByValue("6");
         }
 
-        [Test]
+        [Test,Category("Regression Testing")]
         public void TestMethod2()
         {
             IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
             emailTextField.SendKeys("Selenium C#");
         }
 
-        [Test]
+        [Test,Category("Smoke Testing")]
         public void TestMethod3()
         {
             IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
